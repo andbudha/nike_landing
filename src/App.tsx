81d8cd1951d3404acede26c3400a12595a099479
andbudha <andreibartov@gmail.com>
2024-10-31 13:38:13 +0100

    Nav comp updated/styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Nav from './components/Nav';
 import {
   Hero,
   CustomerReviews,
@@ -10,7 +11,7 @@ import {
 } from './sections';
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
       <Hero />
     </section>

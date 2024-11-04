96e986b391836c89eb1d5a9730aab106cd386e7a
andbudha <andreibartov@gmail.com>
2024-11-04 13:26:10 +0100

    ServiceCard comp created/Services section updated

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -1,5 +1,13 @@
+import SrviceCard from '../components/SrviceCard';
+import { services } from '../constants';
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <SrviceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
